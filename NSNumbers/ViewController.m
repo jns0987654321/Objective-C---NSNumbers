fba86e93c20eee62ea817aff73f2fba7028ec0a7
Junior Samaroo <jns0987654321@gmail.com>
2017-04-29 21:52:26 -0400

    NSNumbers basics done

--- NSNumbers/ViewController.m
+++ NSNumbers/ViewController.m
@@ -16,7 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // dont store native c types, use objects
+    int iAmInt = 5;
+    float iAmFloat = 3.3;
+    double iAmDouble = 5.666667;
+    
+    NSLog(@"Int: %d", iAmInt);
+    NSLog(@"Float: %f", iAmFloat);
+    NSLog(@"int: %f", iAmDouble);
+    
+   // NSArray *arr = @[iAmInt, iAmFloat]; // issues
+    
+    
+    //ALways use NSNumber
+    NSNumber *numInt = [NSNumber numberWithInt:5];
+    //C type
+    int val = numInt.intValue;
+    
+    NSString *strVal = [numInt stringValue];
+    NSString *strVal2 = numInt.stringValue; // dot syntax
+    
+    // better than int because of auto determines type of machine running on
+    NSInteger someInt = 55;
+    
+    // Need value for calculations
+    NSNumber *sumNum = [NSNumber numberWithInt:[numInt intValue] * [numInt intValue]];
+    NSNumber *sumNum2 = [NSNumber numberWithInt:(numInt.intValue * numInt.intValue)];
+
 }
 
 
